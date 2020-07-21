1168cf6c3fcc5995aa153e784e6a17f957e98348
TomMartow <thomasbiggin94+github@gmail.com>
2020-07-22 00:28:38 +0100

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,14 @@ rootProject.name = "nomoreplugins"
 
 include(":nomorewintertodt")
 include(":nomoreagility")
+include(":ahkassistcombat")
+include(":ahkassistskilling")
+include(":ahkgrounditems")
+include(":ahkgroundmarkers")
+include(":ahkinventorytags")
+include(":ahknpchighlight")
+include(":ahkobjectindicators")
+include(":amiscplugin")
 
 for (project in rootProject.children) {
     project.apply {
