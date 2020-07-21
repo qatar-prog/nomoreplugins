1af6b322ee9c53f285f37979c8e4e1b462579af7
TomMartow <thomasbiggin94+github@gmail.com>
2020-07-21 19:25:57 +0100

    test

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "nomoreplugins"
+
+include(":Anonymizer")
+
+for (project in rootProject.children) {
+    project.apply {
+        projectDir = file(name)
+        buildFileName = "${name.toLowerCase()}.gradle.kts"
+
+        require(projectDir.isDirectory) { "Project '${project.path} must have a $projectDir directory" }
+        require(buildFile.isFile) { "Project '${project.path} must have a $buildFile build script" }
+    }
+}
