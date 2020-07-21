941666f1fe1e8c93a6f43a74dc06530dd4445b76
TomMartow <thomasbiggin94+github@gmail.com>
2020-07-21 23:07:25 +0100

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "nomoreplugins"
 
-include(":nomoreagility")
 include(":nomorewintertodt")
+include(":nomoreagility")
 
 for (project in rootProject.children) {
     project.apply {
